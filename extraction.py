7615236587be6efabfc211d2ee3cd585032ef7e7
studybuffalo <studybuffalo@gmail.com>
2017-03-30 18:52:04 -0600

    Updating boilerplate to match license

--- extraction.py
+++ extraction.py
@@ -3,33 +3,27 @@
 
 '''Extracts and saves the Alberta Blue Cross idbl.
 
-  Last Update: 2017-Feb-19
+    Last Update: 2017-Mar-30
 
-  Copyright (c) Notices
-	2017	Joshua R. Torrance	<studybuffalo@studybuffalo.com>
+    Copyright (c) Notices
+	    2017	Joshua R. Torrance	<studybuffalo@studybuffalo.com>
 	
-  This software may be used in any medium or format and adapated to
-  any purpose under the following terms:
-    - You must give appropriate credit, provide a link to the
-      license, and indicate if changes were made. You may do so in
-      any reasonable manner, but not in any way that suggests the
-      licensor endorses you or your use.
-    - You may not use the material for commercial purposes.
-    - If you remix, transform, or build upon the material, you must 
-      distribute your contributions under the same license as the 
-      original.
-	
-  Alternative uses may be discussed on an individual, case-by-case 
-  basis by contacting one of the noted copyright holders.
+    This program is free software: you can redistribute it and/or 
+    modify it under the terms of the GNU General Public License as 
+    published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-  OTHER DEALINGS IN THE SOFTWARE.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, 
+    see <http://www.gnu.org/licenses/>.
+
+    SHOULD YOU REQUIRE ANY EXCEPTIONS TO THIS LICENSE, PLEASE CONTACT 
+    THE COPYRIGHT HOLDERS.
 '''
 
 import sys
