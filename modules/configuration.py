e570b970caa774c3e7a77f9f0d2160fc3a534303
studybuffalo <studybuffalo@gmail.com>
2018-10-21 20:41:37 -0600

    Adding module to handle standard configuration details

---
+++ modules/configuration.py
@@ -0,0 +1,24 @@
+"""Functions and variables for configuration."""
+LOGGING_DICT = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
