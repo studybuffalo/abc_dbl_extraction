701d80d310060bb1505ff422249912fd704fe582
studybuffalo <studybuffalo@gmail.com>
2018-10-21 20:31:33 -0600

    Updatin docstrings

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,3 +1,4 @@
+"""Tests for the run.py file."""
 def test_placeholder():
-    # Placeholder until real tests are written (to stop CI failures)
+    """Placeholder until real tests are written (to stop CI failures)."""
     assert True
