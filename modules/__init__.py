d64eb3746fe8b03967ed2cd05f8162170f0a03f6
studybuffalo <studybuffalo@gmail.com>
2019-07-05 06:55:50 -0600

    Fixing references

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,4 @@
-__all__ = ["extraction", "database", "saving", "website", "debugging"]
+# pylint: disable=missing-docstring
+__all__ = [
+    'configuration', 'database', 'debugging', 'extraction', 'manage', 'saving'
+]
