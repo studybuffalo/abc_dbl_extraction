51de1fb87c47988b3ccafb6042e4a183943c7e01
studybuffalo <studybuffalo@gmail.com>
2019-07-06 08:55:24 -0600

    Initial setup for command line arguments

--- extract.py
+++ extract.py
@@ -1,7 +1,39 @@
 """An Alberta Blue Cross iDBL extraction tool."""
-def set_up_extraction():
-    """Handles initial arguments and configuration to run extraction."""
-    print('test')
+import click
+
+
+@click.command()
+@click.option(
+    '--config', default='extraction.ini', type=click.File(),
+    help='Path to the .ini config file.'
+)
+@click.option(
+    '--disable-data-upload', is_flag=True, help='Disables "data" API upload.'
+)
+@click.option(
+    '--disable-sub-upload', is_flag=True, help='Disables "sub" API upload.'
+)
+@click.option(
+    '--save-url', is_flag=True, help='Save extracted URLs to file.'
+)
+@click.option(
+    '--save-html', is_flag=True, help='Save extracted HTML data to file.'
+)
+@click.option(
+    '--save-api', is_flag=True, help='Save API request data to file.'
+)
+@click.option(
+    '--use-url-file', is_flag=True, help='Uses extracted URL file.'
+)
+@click.option(
+    '--use-html-file', is_flag=True, help='Uses extracted HTML file.'
+)
+def extract(**kwargs):
+    """The Study Buffalo Alberta Blue Cross iDBL Extraction Tool.
+
+        Save locations for files can be specified in the .ini file.
+    """
+    print(kwargs)
 
 if __name__ == '__main__':
-    set_up_extraction()
+    extract() # pylint: disable=no-value-for-parameter
