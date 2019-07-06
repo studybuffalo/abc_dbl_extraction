13b5fd02e3c35133d5ce9a87185ff77842e1ee7f
studybuffalo <studybuffalo@gmail.com>
2019-07-06 07:47:50 -0600

    Creating new initiation script to better handle arguments

---
+++ extract.py
@@ -0,0 +1,7 @@
+"""An Alberta Blue Cross iDBL extraction tool."""
+def set_up_extraction():
+    """Handles initial arguments and configuration to run extraction."""
+    print('test')
+
+if __name__ == '__main__':
+    set_up_extraction()
