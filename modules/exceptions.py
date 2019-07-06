d33b1a3f7abe41c8ae750a809c068edaac50c900
studybuffalo <studybuffalo@gmail.com>
2019-07-06 17:29:59 -0600

    Adding ExtractionException

--- modules/exceptions.py
+++ modules/exceptions.py
@@ -2,3 +2,6 @@
 
 class ImproperlyConfigured(Exception):
     """Exception raised due to improperly configured settings."""
+
+class ExtractionError(Exception):
+    """Exception raised due to issue with HTML extraction."""
